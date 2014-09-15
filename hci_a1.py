6d8acc48927b6c052dc814c2a2f4413544d0d760
jkc8414 <j.k.chan@live.com>
2014-09-15 13:54:54 -0500

    Addded some documentation

--- hci_a1.py
+++ hci_a1.py
@@ -16,30 +16,36 @@ def cancelevent():
     print("cancel")
     sys.exit()
 
+windowHeightNormal = 115
+windowWidth = 310
 offScreen = -9999999
 buttonYCord = 80
 gap = 5
+detailYCord = buttonYCord + detailbutton.sizeHint().height() + gap
+okXCord = detailbutton.sizeHint().width() + gap*4
+cancelXCord = okXCord + gap + okbutton.sizeHint().width()
     
 window = QWidget()
 window.setWindowTitle("Properties Window")
+window.setFixedSize(windowWidth,windowHeightNormal)
 
+# Create and place the image
 advisoryImage = QtGui.QLabel("",window)
 advisoryImage.setPixmap(QPixmap("advisory.jpg"))
 advisoryImage.move(20,20)
 
+# Create and place the primary text label
 textlabel = QtGui.QLabel("Invalid property value",window)
 textlabel.move(30 + advisoryImage.sizeHint().width(),27)
+
+#Create and place the detail text label
 detailString = "The provided fildter string is invalid. The filter\nstring should contain a description of the filter,\nfollowed by the vertical bar (|) and the filter\npattern. The strings for different filtering options\nshould also be separated by the vertical bar.\nExample:\"Text files(*.txt)|*.txt|All files\n(*.*)|*.*\""
 detaillabel = QtGui.QLabel(detailString,window)
 detaillabel.move(0,offScreen)
 
-
+# Create detail button
 detailbutton = QPushButton("D&etails",window)
-detailYCord = buttonYCord + detailbutton.sizeHint().height() + gap
 hidden = 0
-windowHeightNormal = 115
-windowWidth = 310
-window.setFixedSize(windowWidth,windowHeightNormal)
 def toggle():
     global hidden
     if hidden == 0:
@@ -51,15 +57,15 @@ def toggle():
         window.setFixedSize(windowWidth,windowHeightNormal)
         hidden = 0
 
+# Create ok and cancel buttons
 okbutton = QPushButton("Ok",window)
 cancelbutton = QPushButton("Cancel",window)
+# Connect buttons to functions
 okbutton.clicked.connect(okevent)
 cancelbutton.clicked.connect(cancelevent)
 detailbutton.clicked.connect(toggle)
 
-
-okXCord = detailbutton.sizeHint().width() + gap*4
-cancelXCord = okXCord + gap + okbutton.sizeHint().width()
+# Place buttons
 detailbutton.move(5,buttonYCord)
 okbutton.move(okXCord,buttonYCord)
 cancelbutton.move(cancelXCord,buttonYCord)
